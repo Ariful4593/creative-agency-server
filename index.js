ea5fe7fa45c4c4c4830d15a66ae94dd332e5d844
Ariful4593 <arifkhagoria2015@gmail.com>
2020-10-15 20:26:58 +0600

    server side code added

---
+++ index.js
@@ -0,0 +1,135 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
+const fileUpload = require('express-fileupload')
+
+require('dotenv').config()
+
+const app = express();
+app.use(bodyParser.json())
+app.use(cors())
+app.use(fileUpload());
+
+app.get('/', (req, res) => {
+    res.send("Heloo Ariful ISLAM")
+})
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xsirj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const agencyCollection = client.db("creatingAgency").collection("order");
+    const reviewOrder = client.db("creatingAgency").collection("reviewOrder");
+    const adminCollection = client.db("creatingAgency").collection("admin");
+    const addService = client.db("creatingAgency").collection("addService");
+    app.post('/order', (req, res) => {
+
+        const file = req.files.file;
+        const name = req.body.name;
+        const email = req.body.email;
+        const service = req.body.service;
+        const description = req.body.description;
+        const price = req.body.price;
+        
+        const newImg = file.data;
+        const encImg = newImg.toString('base64');
+
+        var image = {
+            contentType: req.files.file.mimetype,
+            size: req.files.file.size,
+            img: Buffer.from(encImg, 'base64')
+        };
+
+        agencyCollection.insertOne({ name,email,service, description,price, image })
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+
+        // const orderData = req.body;
+        // console.log(orderData)
+        // agencyCollection.insertOne(orderData)
+        //     .then(result => {
+        //         res.send(result.insertedCount > 0)
+        //     })
+    })
+
+    app.post('/reviewOrder', (req, res) => {
+        const review = req.body;
+        console.log(review)
+        reviewOrder.insertOne(review)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    app.get('/getReviewOrder', (req, res) =>{
+        reviewOrder.find({})
+        .toArray( (err, documents)  =>{
+            res.send(documents)
+        })
+    })
+
+    app.get('/getOrder', (req, res) =>{
+        // const email = req.body.email;
+        agencyCollection.find({})
+        .toArray( (err, documents) =>{
+            res.send(documents)
+        } )
+    })
+
+    app.post('/getOrderViaEmail', (req, res) =>{
+        const email = req.body.email;
+        agencyCollection.find({email: email})
+        .toArray( (err, documents) =>{
+            res.send(documents)
+        } )
+    })
+
+    app.post('/makeAdmin', (req, res) =>{
+        const email = req.body.email;
+        adminCollection.insertOne({email})
+        .then(result =>{
+            res.send(result.insertedCount > 0)
+        })
+    })
+
+    app.post('/isAdmin', (req,res) =>{
+        const email = req.body.email;
+        adminCollection.find({email: email})
+        .toArray((err, admin) =>{
+            res.send(admin.length > 0)
+        })
+    })
+
+    app.post('/addService', (req, body) =>{
+        const file = req.files.file;
+        const title = req.body.title;
+        const description = req.body.description;
+        const newImg = file.data;
+        const encImg = newImg.toString('base64');
+
+        var image = {
+            contentType: req.files.file.mimetype,
+            size: req.files.file.size,
+            img: Buffer.from(encImg, 'base64')
+        };
+
+        addService.insertOne({ title, description, image })
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
+
+    app.get('/getServiceData', (req, res) =>{
+        addService.find({})
+        .toArray((err, documents) =>{
+            res.send(documents)
+        })
+    })
+});
+
+
+
+const port = 4000;
+app.listen(process.env.PORT || port)
